9227210aac2e9e6539dab38cf35b60f8017e74c7
rydeom <juiplay8@gmail.com>
2024-04-15 18:56:43 +1000

    Hello world

--- src/main.c
+++ src/main.c
@@ -15,90 +15,18 @@
 #include "run_model.h"
 #include <math.h>
 
-#define PIN_MICROPHONE_DISABLE PIN_D5
-
-void startup_callback(void)
-{
-    /* Call this early to put SHTC3 into low power mode */
-    // shtc3_init();
-    /* Disable microphone to reduce current consumption. */
-    riotee_gpio_cfg_output(PIN_MICROPHONE_DISABLE);
-    riotee_gpio_set(PIN_MICROPHONE_DISABLE);
-}
-
 /* This gets called after every reset */
 void reset_callback(void)
 {
-    riotee_gpio_cfg_output(PIN_LED_CTRL);
     riotee_uart_init(PIN_D1, RIOTEE_UART_BAUDRATE_115200);
-    /* Required for VM1010 */
-    riotee_adc_init();
-
-    vm1010_cfg_t cfg = {.pin_vout = PIN_D2, .pin_vbias = PIN_D3, .pin_mode = PIN_D10, .pin_dout = PIN_D4};
-    vm1010_init(&cfg);
 }
-
-/* This gets called when capacitor voltage gets low */
-void turnoff_callback(void)
-{
-    riotee_gpio_clear(PIN_LED_CTRL);
-    /* Disable the microphone */
-    riotee_gpio_set(PIN_MICROPHONE_DISABLE);
-    vm1010_exit();
-}
-int16_t samples[1000];
 
 int main(void)
 {
-    uint64_t startTicks = 0;
-    uint64_t endTicks = 0;
-    printf("Starting up\r\n");
-    riotee_timing_now(&startTicks);
-    run_model(0.8f);
-    riotee_timing_now(&endTicks);
-    printf("Ticks: %llu - %llu = %llu\r\n", endTicks, startTicks, endTicks - startTicks);
-    int rc;
-
+    riotee_wait_cap_charged();
     for (;;)
     {
-        // printf("Starting up\r\n");
-        riotee_sleep_ms(1000);
-        // continue;
-        // riotee_wait_cap_charged();
-
-        /* Switch on microphone */
-        riotee_gpio_clear(PIN_MICROPHONE_DISABLE);
-        /* Wait for 2ms for V_BIAS to come up */
-        riotee_sleep_ticks(70);
-
-        /* Wait for wake-on-sound signal from microphone */
-        if ((rc = vm1010_wait4sound()) != RIOTEE_SUCCESS)
-        {
-            printf("Error while waiting for sound: %d", rc);
-            riotee_gpio_set(PIN_MICROPHONE_DISABLE);
-            continue;
-        }
-        /* Wait until microphone can be sampled (see VM1010 datasheet)*/
-        riotee_sleep_ticks(5);
-        // printf("Sampling..");
-        rc = vm1010_sample(samples, 1000, 4);
-        /* Disable the microphone */
-        riotee_gpio_set(PIN_MICROPHONE_DISABLE);
-        if (rc != RIOTEE_SUCCESS)
-        {
-            printf("Error during sampling: %d", rc);
-        }
-        printf("UU");
-        // Print samples to UART
-        for (int i = 0; i < 1000; i++)
-        {
-            if (samples[i] < -1600)
-            {
-                continue;
-            }
-            riotee_putc((uint8_t)(0x00FF & samples[i]));
-            riotee_putc((uint8_t)(samples[i] >> 8));
-            // printf("%d", samples[i]);
-        }
+        run_model(0.8f);
+        riotee_sleep_ms(500);
     }
 }
